6fbb01fbb33ce8a4d49dd04eb4aa677d160835cf
venkatakrishnagangavarapu <venkatakrishnagangavarapu>
2023-11-08 10:45:25 +0530

    This is my first program in github account

--- src/Practice/A.java
+++ src/Practice/A.java
@@ -1,10 +1,10 @@
 package Practice;
 
-public class sample {
+public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("my first program github account");
+
 	}
 
 }
